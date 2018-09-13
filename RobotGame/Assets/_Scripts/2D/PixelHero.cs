350c6fad4a741e2a79f43a6b471e2f67d3973244
Burton <matthewwburton@gmail.com>
2018-09-13 07:59:04 -0500

    Read last commit

---
+++ RobotGame/Assets/_Scripts/2D/PixelHero.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelHero : MonoBehaviour {
+
+	[SerializeField]private Animator animator;
+
+	[SerializeField]private AnimationClip MoveUpAnimation;
+	[SerializeField]private AnimationClip MoveDownAnimation;
+	[SerializeField]private AnimationClip MoveLeftAnimation;
+	[SerializeField]private AnimationClip MoveRightAnimation;
+
+	public float speed;
+	public float verticalSpeedAdjustment;
+
+	public void MoveUp(){
+		transform.Translate (Vector3.up * (Time.deltaTime * (speed * verticalSpeedAdjustment)), Space.World);
+		animator.Play (MoveUpAnimation.name);
+	}
+
+	public void MoveDown(){
+		transform.Translate (Vector3.down * (Time.deltaTime * (speed * verticalSpeedAdjustment)), Space.World);
+		animator.Play (MoveDownAnimation.name);
+	}
+
+	public void MoveLeft(){
+		transform.Translate (Vector3.left * (Time.deltaTime * speed), Space.World);
+		animator.Play (MoveLeftAnimation.name);
+	}
+
+	public void MoveRight(){
+		transform.Translate (Vector3.right * (Time.deltaTime * speed), Space.World);
+		animator.Play (MoveRightAnimation.name);
+	}
+
+	public void StopMoving(){
+		animator.StopPlayback ();
+	}	
+}
---
+++ RobotGame/Assets/_Scripts/KeyboardHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardHandler : MonoBehaviour {
+
+	[SerializeField] private PixelHero pixelHero;
+
+	void Update () {
+		HandleKeyboard ();
+	}
+
+    void HandleKeyboard() {
+		if (Input.GetKey (KeyCode.W)) {
+			pixelHero.MoveUp ();
+		} 
+
+		else if (Input.GetKey (KeyCode.A)) {
+			pixelHero.MoveLeft ();
+		} 
+
+		else if (Input.GetKey (KeyCode.S)) {
+			pixelHero.MoveDown ();
+		} 
+
+		else if (Input.GetKey (KeyCode.D)) {
+			pixelHero.MoveRight ();
+		} 
+
+		else {
+			pixelHero.StopMoving ();
+		}
+    }
+}
